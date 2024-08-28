7e73431da27482e6e1e8f000f5ad2407d2301081
Matthew Hefferon <matthefferon@gmail.com>
2024-08-27 19:16:03 -0700

    Update color scheme

--- app/page.js
+++ app/page.js
@@ -1,9 +1,8 @@
-// pages/index.js
 import Head from 'next/head';
 
 export default function Home() {
   return (
-    <div className="flex flex-col min-h-screen bg-white">
+    <div className="flex flex-col min-h-screen bg-[#509EE3]">
       <Head>
         <title>Metabase Embed</title>
         <meta name="description" content="Embedding Metabase in Next.js" />
@@ -11,7 +10,7 @@ export default function Home() {
       </Head>
 
       <main className="flex flex-col flex-1 w-full h-full items-center justify-center p-8">
-        <h1 className="text-3xl font-bold mb-4 pb-3 text-gray-800">Metabase Sample Embed</h1>
+        <h1 className="text-3xl font-bold mb-4 pb-3 text-white">Metabase Sample Embed</h1>
         <iframe
           src="http://localhost:3000/public/dashboard/86ba6172-9c36-4d92-88f4-1d070d077081"
           className="flex-1 w-full h-full border-none"
