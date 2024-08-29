2143b734426c0280f6a9bed237507668140a1f3c
Matthew Hefferon <matthefferon@gmail.com>
2024-08-28 18:59:09 -0700

    New example with rounded corners

--- app/page.js
+++ app/page.js
@@ -1,4 +1,4 @@
-import Head from 'next/head';
+import Head from "next/head";
 
 export default function Home() {
   return (
@@ -10,12 +10,13 @@ export default function Home() {
       </Head>
 
       <main className="flex flex-col flex-1 w-full h-full items-center justify-center p-8">
-        <h1 className="text-3xl font-bold mb-4 pb-3 text-white">Metabase Sample Embed</h1>
+        <h1 className="text-3xl font-bold mb-4 pb-3 text-white">
+          Metabase Sample Embed
+        </h1>
         <iframe
-          src="http://localhost:3000/public/dashboard/86ba6172-9c36-4d92-88f4-1d070d077081"
-          className="flex-1 w-full h-full border-none"
+          src="http://localhost:3000/public/dashboard/0dc6f56d-2d6b-498c-b916-e764d4a4dea5"
+          className="flex-1 w-full h-full border-none rounded-lg"
           allowTransparency
-          allowFullScreen
         ></iframe>
       </main>
     </div>
